6ecc014876605459e377cd9920ca327be1a15d66
jabad88 <abadiesjosh@gmail.com>
2024-02-21 14:01:16 -0800

    Added try/except block to check ticker

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://finance.yahoo.com/quote/SPY/'
-r = requests.get(url).text
-soup = BeautifulSoup(r,'html.parser')
+stocks = []
+collecting_tickers = True
 
-#Get specific information
-price = soup.find('fin-streamer', {'class': "Fw(b) Fz(36px) Mb(-4px) D(ib)"}).text
-change = soup.find('fin-streamer', {'class': "Fw(500) Pstart(8px) Fz(24px)"}).text
-change_percent = soup.find_all('fin-streamer', {"class":"Fw(500) Pstart(8px) Fz(24px)"})[1].text
-ticker = soup.find('h1',{'class':"D(ib) Fz(18px)"}).text
-
-print(ticker, price, change, change_percent)
+while collecting_tickers:
+    tickers = input('Please input the stock tickers you would like to track one at a time. When finished type "exit". ').upper()
+    if tickers != "EXIT":
+        stocks.append(tickers)
+    else:
+        collecting_tickers = False
 
 
-#Add list to get info on other stocks, input from user
-#Create a dictionary, csv, json from data.
+for x in stocks:
+    url = f'https://finance.yahoo.com/quote/{x}/'
+    r = requests.get(url).text
+    soup = BeautifulSoup(r,'html.parser')
+
+    try:
+        #Get specific information
+        price = soup.find('fin-streamer', {'class': "Fw(b) Fz(36px) Mb(-4px) D(ib)"}).text
+        change = soup.find('fin-streamer', {'class': "Fw(500) Pstart(8px) Fz(24px)"}).text
+        change_percent = soup.find_all('fin-streamer', {"class":"Fw(500) Pstart(8px) Fz(24px)"})[1].text
+        ticker = soup.find('h1',{'class':"D(ib) Fz(18px)"}).text
+
+        print("\n",ticker, price, change, change_percent)
+
+    except AttributeError:
+        print("\n",f"{x} is not a valid ticker.")
+
+
